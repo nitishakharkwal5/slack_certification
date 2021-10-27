b106845d708e18b971af95647e178c45e0e722f9
nitishakharkwal5 <nitishakharkwal5@gmail.com>
2021-10-27 20:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.post("/items/")
 async def create_item(item: Item):
     return item
